fc411d144ee8323c39898d7dfa72c013d1f2019b
Stef Corputty <scorputty@gmail.com>
2017-02-18 19:46:12 +0100

    fixed json

--- rundocker.sh
+++ rundocker.sh
@@ -4,6 +4,9 @@
 VOL_SHARE="/Volumes/shares/docker/"
 
 test -d ${VOL_SHARE} || VOL_SHARE="${PWD}${VOL_SHARE}" && mkdir -p ${VOL_SHARE}/config/transmission
+test -d ${VOL_SHARE}/media/Downloads/transmission/complete || VOL_SHARE="${PWD}${VOL_SHARE}/media/Downloads/transmission/complete" && mkdir -p ${VOL_SHARE}/media/Downloads/transmission/complete
+test -d ${VOL_SHARE}/media/Downloads/transmission/incomplete || VOL_SHARE="${PWD}${VOL_SHARE}/media/Downloads/transmission/incomplete" && mkdir -p ${VOL_SHARE}/media/Downloads/transmission/incomplete
+test -d ${VOL_SHARE}/media/Downloads/transmission/watch || VOL_SHARE="${PWD}${VOL_SHARE}/media/Downloads/transmission/watch" && mkdir -p ${VOL_SHARE}/media/Downloads/transmission/watch
 
 docker run -d -h $(hostname) \
     -e USER=media -e USERID=10000 \
